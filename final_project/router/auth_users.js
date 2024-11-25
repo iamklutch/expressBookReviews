f27d8bb82f3606a370f0f1b008a7c6887bf6b173
James Higashiyama <james.higashiyama@gmail.com>
2024-11-25 09:29:31 -0700

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,56 +1,85 @@
-import user from "../models/user.js";
-import { hashPassword, compare_hashed_passwords } from "../utils/hashing.js";
-import { createToken } from "../utils/tokens.js"
+const express = require('express');
+const jwt = require('jsonwebtoken');
+let books = require("./booksdb.js");
+const regd_users = express.Router();
 
-export async function register(req, res) {
-    try {
-        const { username, password } = req.body;
+let users = [{"username":"dennis","password":"abc"}];
 
-        // check if the user name found
-        const foundUser = await user.findOne({ where: { username } });
-        if (foundUser) {
-            return res.json({ message: "This user is already registered!" });
-        }
-
-        // hashing the password
-        const hashedPassword = await hashPassword(password);
-
-        // execute the registration
-        const newUser = await user.create({ username: username, password: hashedPassword });
-
-        res.json({ message: "User registered successfully!" });
-    } catch (error) {
-
-        res.status(500).json({ message: "Internal Server Error!!" });
-
-    }
+const isValid = (username)=>{ //returns boolean
+//write code to check is the username is valid
+    const userMatches = users.filter((user) => user.username === username);
+    return userMatches.length > 0;
 }
 
-export async function login(req, res) {
-    try {
+const authenticatedUser = (username,password)=>{ //returns boolean
+//write code to check if username and password match the one we have in records.
+  const matchingUsers = users.filter((user) => user.username === username && user.password === password);
+  return matchingUsers.length > 0;
+}
 
-        const { username, password } = req.body;
 
-        // check if user registered or not
-        const registeredUser = await user.findOne({ where: { username } });
-        if (!registeredUser) {
-            return res.json({ message: "Invalid Credentials!" });
+
+//only registered users can login
+
+regd_users.post("/login", (req,res) => {
+  //Write your code here
+  console.log("login: ", req.body);
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(404).json({message: "Error logging in"});
+  }
+
+  if (authenticatedUser(username,password)) {
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60 * 60 });
+
+    req.session.authorization = {
+            accessToken,username
         }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({message: "Invalid Login. Check username and password"});
+    }
+});
 
-        // check password matching
-        const is_matched = await compare_hashed_passwords(password, registeredUser.password);
-        if (!is_matched) {
-            return res.json({ message: "Invalid Credentials!" });
-        }
+// Add a book review
+regd_users.put("/auth/review/:isbn", (req, res) => {
+  //Write your code here
+    const isbn = req.params.isbn;
+    const review = req.body.review;
+    const username = req.session.authorization.username;
+    console.log("add review: ", req.params, req.body, req.session);
+    if (books[isbn]) {
+        let book = books[isbn];
+        book.reviews[username] = review;
+        return res.status(200).send("Review successfully posted");
+    }
+    else {
+        return res.status(404).json({message: `ISBN ${isbn} not found`});
+    }
+});
 
-        // create token
-        const token = createToken(registeredUser.id, username);
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const username = req.session.authorization.username;
+  if (books[isbn]) {
+      let book = books[isbn];
+      delete book.reviews[username];
+      return res.status(200).send("Review successfully deleted");
+  }
+  else {
+      return res.status(404).json({message: `ISBN ${isbn} not found`});
+  }
+});
 
-        return res.json({ message: "User logged in successfully!", token });
 
-    } catch (error) {
 
-        res.status(500).json({ message: "Internal Server Error!!" });
+module.exports.authenticated = regd_users;
+module.exports.isValid = isValid;
+module.exports.users = users;
 
     }
 }
