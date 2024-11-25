3e1b0756bda55ebcef7a627f428075c74107162a
James Higashiyama <james.higashiyama@gmail.com>
2024-11-25 09:25:52 -0700

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,30 +1,56 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-let books = require("./booksdb.js");
-const regd_users = express.Router();
+import user from "../models/user.js";
+import { hashPassword, compare_hashed_passwords } from "../utils/hashing.js";
+import { createToken } from "../utils/tokens.js"
 
-let users = [];
+export async function register(req, res) {
+    try {
+        const { username, password } = req.body;
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+        // check if the user name found
+        const foundUser = await user.findOne({ where: { username } });
+        if (foundUser) {
+            return res.json({ message: "This user is already registered!" });
+        }
+
+        // hashing the password
+        const hashedPassword = await hashPassword(password);
+
+        // execute the registration
+        const newUser = await user.create({ username: username, password: hashedPassword });
+
+        res.json({ message: "User registered successfully!" });
+    } catch (error) {
+
+        res.status(500).json({ message: "Internal Server Error!!" });
+
+    }
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+export async function login(req, res) {
+    try {
+
+        const { username, password } = req.body;
+
+        // check if user registered or not
+        const registeredUser = await user.findOne({ where: { username } });
+        if (!registeredUser) {
+            return res.json({ message: "Invalid Credentials!" });
+        }
+
+        // check password matching
+        const is_matched = await compare_hashed_passwords(password, registeredUser.password);
+        if (!is_matched) {
+            return res.json({ message: "Invalid Credentials!" });
+        }
+
+        // create token
+        const token = createToken(registeredUser.id, username);
+
+        return res.json({ message: "User logged in successfully!", token });
+
+    } catch (error) {
+
+        res.status(500).json({ message: "Internal Server Error!!" });
+
+    }
 }
-
-//only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
-
-// Add a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
-
-module.exports.authenticated = regd_users;
-module.exports.isValid = isValid;
-module.exports.users = users;
